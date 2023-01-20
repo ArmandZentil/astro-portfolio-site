14c594971d3303c9b992e52127855852f6254f03
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-01-20 15:07:19 +0200

    price to free

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import compress from "astro-compress";
-import partytown from "@astrojs/partytown";
+
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://supremeuikits.com',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+   site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), image(), compress(),  sitemap()]
 });
